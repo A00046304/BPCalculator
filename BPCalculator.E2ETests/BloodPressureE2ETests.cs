67e374b4e023b97c978e82e4254c4110d2d75619
HARSHITH ALUR LAKSHMANAGOWDA <A00046304@myTUDublin.ie>
2025-12-09 15:17:31 +0530

    cd testing e2e fix 2

--- BPCalculator.E2ETests/BloodPressureE2ETests.cs
+++ BPCalculator.E2ETests/BloodPressureE2ETests.cs
@@ -23,7 +23,7 @@ namespace BPCalculator.E2ETests
 
             _driver = new ChromeDriver(options);
 
-            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(15));
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(20));
             _wait.IgnoreExceptionTypes(typeof(StaleElementReferenceException));
 
             _baseUrl = Environment.GetEnvironmentVariable("BP_E2E_BASEURL")
